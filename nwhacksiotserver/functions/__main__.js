1018ac662acc970a89798475dc6f12a254303479
Michael Englo <menglo@optimalefficiency.net>
2018-01-14 09:48:52 -0800

    add send sms functions and counter increment & loop

--- nwhacksiotserver/functions/__main__.js
+++ nwhacksiotserver/functions/__main__.js
@@ -14,7 +14,7 @@ module.exports = (data = null, context, callback) => {
     }
 
     let newCounter = value || 0;
-    newCounter++;
+    console.log("newCounter before: " + newCounter);
 
     const key1 = '157ba96148d473186e101910b708b01';
     const key2 = 'ec8085b928134a11919817626ba6404b';
@@ -25,7 +25,7 @@ module.exports = (data = null, context, callback) => {
         'Ocp-Apim-Subscription-Key':  key2,
       },
       body: {
-        url: "https://s3.ca-central-1.amazonaws.com/nwhacksiot/friends/donald-trump/9.jpg"
+        url: "https://upload.wikimedia.org/wikipedia/commons/2/21/EverestfromKalarPatarcrop.JPG"
       },
       json: true
     }, (err,httpResponse,body) => {
@@ -34,7 +34,26 @@ module.exports = (data = null, context, callback) => {
       }
 
       if(body.length == 0) {
-        return callback(null, "I'm not recognizing any faces. do something");
+        if (newCounter >= 9) {
+          return lib.messagebird.tel['@0.0.10'].sms({
+            originator: '14509905506',
+            recipient: '17787062154',
+            body: 'unknown moving object detected! View on this link:'
+          }, (err, result) => {
+            //reset counter to 0
+            newCounter = 0;
+            console.log('newCounter after: ' + newCounter);
+            return lib.utils.storage.set('COUNTER', newCounter, (err, value) => {
+              return callback(null, "I'm not recognizing any faces");
+            });
+          });
+        } else {
+          newCounter++;
+          console.log('newCounter after: ' + newCounter);
+          return lib.utils.storage.set('COUNTER', newCounter, (err, value) => {
+            return callback(null, "I'm not recognizing any faces");
+          });
+        }
       }
       request.post({
         url: 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/identify',
@@ -53,18 +72,40 @@ module.exports = (data = null, context, callback) => {
         }
 
         if (httpResponse.body[0].candidates.length != 0) {
-          return callback(null, "that's someone i know!");
+            if (newCounter >= 0)
+              newCounter -= 10;
+            else
+              newCounter++;            
+            console.log("newCounter after: " + newCounter);
+            
+            lib.utils.storage.set('COUNTER', newCounter, (err, value) => {
+              return callback(null, "that's someone i know!");
+            });
         }
         else {
-          return callback(null, "i don't recognize him/her"); 
+          if (newCounter >= 9) {
+            lib.messagebird.tel['@0.0.10'].sms({
+              originator: '14509905506',
+              recipient: '17787062154',
+              body: 'unknown moving object detected! View on this link:'
+            }, (err, result) => {
+              //reset counter to 0
+              newCounter = 0;
+              console.log('newCounter after: ' + newCounter);
+              lib.utils.storage.set('COUNTER', newCounter, (err, value) => {
+                return callback(null, "i don't recognize him/her");
+              });
+            });
+          }
+          else {
+            newCounter++;
+            console.log('newCounter after: ' + newCounter);
+            lib.utils.storage.set('COUNTER', newCounter, (err, value) => {
+              return callback(null, "i don't recognize him/her");
+            });
+          }
         }
-
-        callback(null, JSON.stringify(httpResponse));
       });
     });
-
-    // lib.utils.storage.set('COUNTER', newCounter, (err, value) => {
-      //callback(null, httpResponse);
-    // });
   });
 };
